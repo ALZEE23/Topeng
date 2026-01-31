cd0139455533db77beae4b1d523bf33f132bb883
ALZEE23 <muhamadalfarizial726@gmail.com>
2026-01-31 15:09:46 +0700

    feat: add smoother animation

--- Assets/Script/GamblingManager.cs
+++ Assets/Script/GamblingManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,21 @@ public class GamblingManager : MonoBehaviour
     public Animator enemyAnimator;
     public Animator playerAnimator;
     public bool isPlayerTurn = false;
+    // public GameObject CameraGambling;
+    public GameObject CameraPov;
+
+    public bool isMaskedUsed = false;
+    public bool isSpawnedDice = false;
+    public bool isSpawnedEnemyDice = false;
+
+    public Transform diceSpawnPoint;
+    public Transform diceEnemySpawnPoint;
+
+    public List<DiceObject> diceObjects;
+    public int dificultyLevel = 1;
+
+    public
+
     void Start()
     {
         // TurnOnGambling();
@@ -16,11 +32,16 @@ public class GamblingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0) && isSpawnedEnemyDice == true)
+        {
+            OnMaskedUsed();
+        }
     }
 
     public void TurnOnGambling()
     {
+        isSpawnedDice = false;
+        isSpawnedEnemyDice = false;
         Debug.Log("Gambling Turned On");
         if (isPlayerTurn)
         {
@@ -33,23 +54,33 @@ public class GamblingManager : MonoBehaviour
 
     }
 
-    public void OnMaskedUsed(bool OnOff)
+    public void OnMaskedUsed()
     {
         Debug.Log("Masked Used in Gambling");
-        if (isPlayerTurn)
+        if (isPlayerTurn == true)
         {
-            playerAnimator.SetBool("isMasked", OnOff);
+            enemyAnimator.SetTrigger("UseMask");
         }
         else
         {
-            enemyAnimator.SetBool("isMasked", OnOff);
+            playerAnimator.SetTrigger("UseMask");
+            isMaskedUsed = true;
+            CameraPov.SetActive(true);
         }
     }
 
     public void OnShuffleDice(Animator animator)
     {
-        animator.SetTrigger("isShuffle");
+        animator.SetTrigger("IsShuffle");
+
+        StartCoroutine(WaitShuffleToSpawnDice());
         Debug.Log("Dice Shuffled");
+    }
+
+    public IEnumerator WaitShuffleToSpawnDice()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SpawnDice();
     }
 
     public void OnShowDiceResult()
@@ -63,12 +94,75 @@ public class GamblingManager : MonoBehaviour
         {
             isPlayerTurn = false;
         }
-        OnMaskedUsed(false);
+        // OnMaskedUsed();
         TurnOnGambling();
     }
 
+    // player start gambling or guesing
+    public void OnGambling()
+    {
+        Debug.Log("Gambling Started");
+        // player 
+        if (isPlayerTurn)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnUnmask()
+    {
+        Debug.Log("Unmasking");
+        if (isPlayerTurn)
+        {
+            playerAnimator.SetTrigger("MaskOff");
+            CameraPov.SetActive(false);
+        }
+        else
+        {
+            enemyAnimator.SetTrigger("MaskOff");
+        }
+
+
+        //testing 
+        OnShowDiceResult();
+    }
+
+    public IEnumerator WaitUnmaskAfterSpawnDice()
+    {
+        yield return new WaitForSeconds(2.0f);
+        OnUnmask();
+    }
+
+    // spawn prefab dice
     public void SpawnDice()
     {
         Debug.Log("Dice Spawned");
+        if (isPlayerTurn)
+        {
+            isSpawnedDice = true;
+            OnMaskedUsed();
+            Debug.Log("Player Dice Spawned");
+            //spawn logic here
+            StartCoroutine(WaitUnmaskAfterSpawnDice());
+        }
+        else
+        {
+            isSpawnedEnemyDice = true;
+            Debug.Log("Enemy Dice Spawned");
+            StartCoroutine(WaitUnmaskAfterSpawnDice());
+            //spawn logic here
+        }
     }
+
+}
+
+[System.Serializable]
+public class DiceObject
+{
+    public int diceValue;
+    public GameObject diceGameObject;
 }
